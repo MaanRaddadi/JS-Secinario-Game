e9bb2c5103ecd30ee6584d60d9cb4486b21f1171
MaanRaddadi <maan.raddadi@gmail.com>
2023-11-11 13:17:30 +0300

    -Added Second secenario

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const stores = [
+  {
+    name: "Store A",
+    location: "First-Floor",
+    prices: "1000",
+  },
+  {
+    name: "Store B",
+    location: "Second-Floor",
+    prices: "2500",
+  },
+  {
+    name: "Store C",
+    location: "Ground-Fall",
+    prices: "5500",
+  },
+  {
+    name: "Store D",
+    location: "First-Floor",
+    prices: "900",
+  },
+  {
+    name: "Store E",
+    location: "Third-Floor",
+    prices: "800",
+  },
+];
+
+//Game Enviroment For Each Level
+let mainContainer = document.querySelector(".main-container");
+let marketEnviroment = document.querySelector(".enviroment-contianer");
+let storesEnviroment = document.querySelector(".stores-enviroment");
+
 const questions = [
   {
     questionTitle: "اكتب دالة تستخرج قيمة 'Faisal'  وترجعه لأهله",
@@ -6,6 +39,49 @@ const questions = [
       const store = ["Khaled","Nora","Mohammed","Faisal","Sarah"];
        //type your code here 
       }`,
+    enviroment: marketEnviroment,
+    animation: "FirstAnswerAnimation",
+    target: "kid",
+    arg: "",
+  },
+  {
+    questionTitle:
+      "ستخرج جميع اسماء المتاجر المناسبة لميزانية نورة (اسماء المتاجر يجب ان تكون بصيغة نصية String)",
+    keyAnswer: "Store A Store D Store E",
+    questionHelperCoder: `function findCheapStores(){ 
+      const stores = [
+        {
+          name: "Store A",
+          location: "First-Floor",
+          prices: "1000",
+        },
+        {
+          name: "Store B",
+          location: "Second-Floor",
+          prices: "2500",
+        },
+        {
+          name: "Store C",
+          location: "Ground-Fall",
+          prices: "5500",
+        },
+        {
+          name: "Store D",
+          location: "First-Floor",
+          prices: "900",
+        },
+        {
+          name: "Store E",
+          location: "Third-Floor",
+          prices: "800",
+        },
+      ];
+      //type your code here 
+          }`,
+    enviroment: storesEnviroment,
+    animation: "SecondAnswerAnimation",
+    target: "man-stores",
+    arg: stores,
   },
 ];
 //Global Variables
@@ -13,11 +89,11 @@ const modalTitle = document.querySelector(".modal-title");
 const modalBody = document.querySelector(".modal-body");
 let questionText = document.querySelector(".question");
 let runBtn = document.getElementById("run");
-let gameEnviroment = document.querySelector(".enviroment-contianer");
-let mainContainer = document.querySelector(".main-container");
+let dialogBox = document.querySelectorAll(".dialog-box");
 let editorContainer = document.querySelector(".editor-container");
 let modalBtn = document.querySelector(".modal-btn");
 let dialogText = document.getElementById("dialog-text");
+let nextBtn = document.querySelector(".modal-btn-next");
 let currentQuestion = 0;
 // Setting Up the code editor
 var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("editor"), {
@@ -25,11 +101,8 @@ var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("editor"), {
   theme: "dracula",
   lineNumbers: true,
 });
-gameEnviroment.style.display = "none";
-editorContainer.style.display = "none";
 
-startTheGame();
-
+// Define Modal elements to change based on the users answer
 var myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
 var currectAnswerModal = new bootstrap.Modal(
   document.getElementById("currectAnswerModal")
@@ -37,6 +110,8 @@ var currectAnswerModal = new bootstrap.Modal(
 var wrongAnswerModel = new bootstrap.Modal(
   document.getElementById("wrongAnswerModel")
 );
+
+startTheGame();
 // Setting up the start of the game
 function startTheGame() {
   myCodeMirror.setValue(questions[0].questionHelperCoder);
@@ -58,31 +133,49 @@ function startTheGame() {
   };
 }
 modalBtn.addEventListener("click", () => {
-  gameEnviroment.style.display = "block";
-  editorContainer.style.display = "block";
   myModal.hide();
 });
 
 // Run Button Event
 
 runBtn.addEventListener("click", () => {
-  // Define Modal elements to change based on the users answer
- 
-    const userCode = myCodeMirror.getValue();
-    const userResult = eval(`(${userCode})()`);
-    checkAnswer(userResult);
-  
+  const userCode = myCodeMirror.getValue();
+  const userResult = eval(`(${userCode})()`);
+  checkAnswer(userResult);
 });
 
 function checkAnswer(result) {
-  // setting the animation if the answe is currect
+  // setting the animation if the answer is currect
   // show the currectAnswer Modal after the animation ends
   if (result === questions[currentQuestion].keyAnswer) {
-    document.getElementById("kid").classList.toggle("FirstAnswerAnimation");
+    document
+      .getElementById(questions[currentQuestion].target)
+      .classList.toggle(questions[currentQuestion].animation);
+      //setting a fadeout animation for the dialog box
+    dialogBox[currentQuestion].classList.toggle("fadeOut");
+    if (currentQuestion === 1) {
+      setTimeout(() => {
+        document.querySelector(".dialog-box-fade-in").style.display = "block";
+      }, 5000);
+    }
+    //showing the currect answer modal after a delay
     setTimeout(() => {
       currectAnswerModal.show();
-    }, 5000);
+    }, 6500);
   } else {
     wrongAnswerModel.show();
   }
+}
+//event for moving to the next secenario
+nextBtn.addEventListener("click", () => {
+  questions[currentQuestion].enviroment.style.display = "none";
+  currentQuestion++;
+  currectAnswerModal.hide();
+  showNextQuestion();
+});
+
+function showNextQuestion() {
+  questions[currentQuestion].enviroment.style.display = "block";
+  questionText.textContent = questions[currentQuestion].questionTitle;
+  myCodeMirror.setValue(questions[currentQuestion].questionHelperCoder);
 }
