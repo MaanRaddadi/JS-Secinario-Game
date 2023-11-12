3c634488b97d9d1d25c1c4ee2f69607338347b7d
MaanRaddadi <maan.raddadi@gmail.com>
2023-11-12 07:35:34 +0300

    -Added fourth enviroment

--- script.js
+++ script.js
@@ -2,34 +2,43 @@ const stores = [
   {
     name: "Store A",
     location: "First-Floor",
-    prices: "1000",
+    prices: 1000,
   },
   {
     name: "Store B",
     location: "Second-Floor",
-    prices: "2500",
+    prices: 2500,
   },
   {
     name: "Store C",
     location: "Ground-Fall",
-    prices: "5500",
+    prices: 5500,
   },
   {
     name: "Store D",
     location: "First-Floor",
-    prices: "900",
+    prices: 900,
   },
   {
     name: "Store E",
     location: "Third-Floor",
-    prices: "800",
+    prices: 800,
   },
+];
+
+//Define answer for comparison
+const sortedBooks = [
+  { title: "Book B", author: "Author B", year: 1998 },
+  { title: "Book A", author: "Author A", year: 2005 },
+  { title: "Book C", author: "Author C", year: 2010 },
 ];
 
 //Game Enviroment For Each Level
 let mainContainer = document.querySelector(".main-container");
 let marketEnviroment = document.querySelector(".enviroment-contianer");
 let storesEnviroment = document.querySelector(".stores-enviroment");
+let roomEnviroment = document.querySelector(".room-enviroment");
+let secondRoomEnviroment = document.querySelector(".second-room-enviroment");
 
 const questions = [
   {
@@ -42,7 +51,7 @@ const questions = [
     enviroment: marketEnviroment,
     animation: "FirstAnswerAnimation",
     target: "kid",
-    arg: "",
+    winningDialog: "مبرررررررررررروك, وفيصل رجع لأهله",
   },
   {
     questionTitle:
@@ -82,6 +91,39 @@ const questions = [
     animation: "SecondAnswerAnimation",
     target: "man-stores",
     arg: stores,
+    winningDialog: "حلك اسطوووري",
+  },
+  {
+    questionTitle:
+      "اكتب دالة تستخرج الارقام فقط من القيمة النصية المعطاة وترجعها في قيمة نصية منفصلة",
+    keyAnswer: "050020093920",
+    questionHelperCoder: `function getNumber(){ 
+      const meassage = "Ex#0Rb/5pT0Q0bZ#20!a0$!9*3+9()20";
+       //type your code here 
+      }`,
+    enviroment: roomEnviroment,
+    animation: "SecondAnswerAnimation",
+    target: "man-stores",
+    winningDialog: "ماشاء الله, واضح انك ادفانسد",
+  },
+  {
+    questionTitle:
+      "اكتب دالة ترتب الكتب من الاقدم إلى الأحدث وترجع القيمة كمصفوفة",
+    keyAnswer: JSON.stringify(sortedBooks),
+
+    questionHelperCoder: `function sortBooks(){ 
+  const books = [
+    { title: 'Book A', author: 'Author A', year: 2005 },
+    { title: 'Book B', author: 'Author B', year: 1998 },
+    { title: 'Book C', author: 'Author C', year: 2010 },
+  ]
+   //type your code here 
+
+  }`,
+    enviroment: secondRoomEnviroment,
+    animation: "SecondAnswerAnimation",
+    target: "man-stores",
+    winningDialog: "انتهت اللعبة",
   },
 ];
 //Global Variables
@@ -94,6 +136,7 @@ let editorContainer = document.querySelector(".editor-container");
 let modalBtn = document.querySelector(".modal-btn");
 let dialogText = document.getElementById("dialog-text");
 let nextBtn = document.querySelector(".modal-btn-next");
+let currectAnswerModalText = document.querySelector(".currectAnswer-text");
 let currentQuestion = 0;
 // Setting Up the code editor
 var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("editor"), {
@@ -141,7 +184,11 @@ modalBtn.addEventListener("click", () => {
 runBtn.addEventListener("click", () => {
   const userCode = myCodeMirror.getValue();
   const userResult = eval(`(${userCode})()`);
-  checkAnswer(userResult);
+  if (currentQuestion === 3) {
+    checkAnswer(JSON.stringify(userResult));
+  } else {
+    checkAnswer(userResult);
+  }
 });
 
 function checkAnswer(result) {
@@ -151,27 +198,37 @@ function checkAnswer(result) {
     document
       .getElementById(questions[currentQuestion].target)
       .classList.toggle(questions[currentQuestion].animation);
-      //setting a fadeout animation for the dialog box
+    //setting a fadeout animation for the dialog box
     dialogBox[currentQuestion].classList.toggle("fadeOut");
     if (currentQuestion === 1) {
       setTimeout(() => {
         document.querySelector(".dialog-box-fade-in").style.display = "block";
       }, 5000);
     }
+    if (currentQuestion === 3) {
+      document.getElementById("book1").style.animationName = "booksAnimation1";
+      document.getElementById("book2").style.animationName = "booksAnimation2";
+      document.getElementById("book3").style.animationName = "booksAnimation3";
+    }
     //showing the currect answer modal after a delay
+    currectAnswerModalText.innerText = questions[currentQuestion].winningDialog;
     setTimeout(() => {
       currectAnswerModal.show();
-    }, 6500);
+    }, 6000);
   } else {
     wrongAnswerModel.show();
   }
 }
 //event for moving to the next secenario
 nextBtn.addEventListener("click", () => {
-  questions[currentQuestion].enviroment.style.display = "none";
-  currentQuestion++;
-  currectAnswerModal.hide();
-  showNextQuestion();
+  if (currentQuestion < 3) {
+    questions[currentQuestion].enviroment.style.display = "none";
+    currentQuestion++;
+    currectAnswerModal.hide();
+    showNextQuestion();
+  } else {
+    currectAnswerModal.hide();
+  }
 });
 
 function showNextQuestion() {
